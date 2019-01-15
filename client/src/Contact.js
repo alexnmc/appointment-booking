7f14c3f8ff29151590aff94aa84b3509cf8e1283
Alexander <nemechekalexander@gmail.com>
2019-01-15 18:55:17 -0500

    update

--- client/src/Contact.js
+++ client/src/Contact.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
     return (
         <div className="contact">
-            <p className = "aboutText">Contact us by email: nemecheckalexander@gmail.com</p>
+            <p className = "aboutText2">Contact us by email: nemecheckalexander@gmail.com</p>
         </div>
     )
 }
