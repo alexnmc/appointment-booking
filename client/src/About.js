fb798c24b95628086e2b08b558ec68cef6eb5dc1
Alexander <nemechekalexander@gmail.com>
2019-01-27 04:03:02 +0200

    update

--- client/src/About.js
+++ client/src/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
     return (
         <div className="about">
-            <p className = "aboutText">We provide a very user friendly appointment booking platform able to manage bookings for any type of business.Our blueprint aplication is ready to be customized to your operational needs. </p>
+            <p className = "aboutText">We provide a very user friendly appointment booking platform able to manage bookings for any type of business. </p>
             
         </div>
     )
