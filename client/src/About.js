d03740757d41d11671311c43bd54c7cd15da709c
Alexander <nemechekalexander@gmail.com>
2019-04-10 01:51:25 +0300

    update

--- client/src/About.js
+++ client/src/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
     return (
         <div className="about">
-            <p className = "aboutText">We provide a very user friendly appointment booking platform able to manage bookings for any type of business. </p>
+            <p className = "aboutText">Jet Ski Rental</p>
             
         </div>
     )
--- client/src/Contact.js
+++ client/src/Contact.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
     return (
         <div className="contact">
-            <p className = "aboutText2">Contact us by email: nemecheckalexander@gmail.com</p>
+            <p className = "aboutText2"> nemecheckalexander@gmail.com</p>
         </div>
     )
 }
