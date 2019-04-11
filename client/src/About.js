e4b4c18da1c8b67b1439cf243be8197007db72d9
Alexander <nemechekalexander@gmail.com>
2019-04-11 19:39:45 +0300

    update

--- client/src/About.js
+++ client/src/About.js
@@ -3,7 +3,8 @@ import React from 'react'
 const About = () => {
     return (
         <div className="about">
-            <p className = "aboutText">Jet Ski Rental</p>
+           
+            <p className = "aboutText">use code vschool to sign up as admin and view the database</p>
             
         </div>
     )
