4961ea3b5e53b42a5da6172c0c70cfb018a0e393
Alexander <nemechekalexander@gmail.com>
2019-05-25 15:34:51 +0300

    select and options added for admin

--- client/src/Loading.js
+++ client/src/Loading.js
@@ -6,7 +6,7 @@ const Loading = ({type, color}) => {
     
   return(
     <div className = "loading">
-      <ReactLoading  type={'spinningBubbles'} color={'rgb(252, 107, 10)'} height={'35%'} width={'35%'}/>
+      <ReactLoading  type={'spin'} color={'rgb(252, 107, 10)'} height={'35%'} width={'35%'}/>
     </div>
   )
 }
